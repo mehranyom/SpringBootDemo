633ec38991ffde0f461fbdf7025408275f819148
Ali Adham <ali.adham@studenti.polito.it>
2025-07-30 23:30:50 +0200

    actuator

--- src/main/java/com/mehran/first/demo/app/rest/FunRestController.java
+++ src/main/java/com/mehran/first/demo/app/rest/FunRestController.java
@@ -1,13 +1,27 @@
 package com.mehran.first.demo.app.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 
 
 @RestController
 public class FunRestController {
     //expose "/" that return "Hello World"
+
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Team Coach: " + coachName + " and Team Name: " + teamName;
+    }
+    
 
     @GetMapping("/")
     public String sayHello() {
